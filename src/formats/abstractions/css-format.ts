532075e86683741956c5da4429dca8e9373ec6b9
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-12-14 18:19:31 +0200

    Change properties of CSSFormat instances

--- src/formats/abstractions/css-format.ts
+++ src/formats/abstractions/css-format.ts
@@ -1,6 +1,5 @@
 import { FormatStructure } from "format-structure";
 
 export abstract class CSSFormat extends FormatStructure {
-    abstract propertyName: string;
-    abstract propertyValue: string;
+    abstract className: string;
 }
